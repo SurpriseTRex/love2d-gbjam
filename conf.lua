1f514dd3c7801585b4cd0301d70d3ac425a8e6a4
Sean Johnston <sean.c.johnston@gmail.com>
2016-10-01 20:07:45 +0100

    add tiled loading, and sprite movement with WASD

--- conf.lua
+++ conf.lua
@@ -1,8 +1,8 @@
 function love.conf(t)
   t.title = "GBJam Game"
   t.version = "0.10.1"
-  t.window.width = 160
-  t.window.height = 144
+  t.window.width = 480
+  t.window.height = 432
 
   t.console = true
 end
--- main.lua
+++ main.lua
@@ -1,17 +1,60 @@
-debug = true
+local sti = require "sti"
 
-player = {x = 23, y = 30, speed = 2}
+function love.load()
+    windowWidth  = love.graphics.getWidth()
+    windowHeight = love.graphics.getHeight()
 
-function love.load(args)
-  position = {x = 0, y = 0}
+    map = sti("assets/testmap.lua")
+    map:addCustomLayer("Sprite Layer", 3)
+
+    local spriteLayer = map.layers["Sprite Layer"]
+
+    spriteLayer.sprites = {
+        player = {
+            image = love.graphics.newImage("assets/player.png"),
+            x = 64,
+            y = 64,
+            r = 0,
+        }
+    }
+
+    function spriteLayer:update(dt)
+      local y_diff = 0
+      local x_diff = 0
+
+      if love.keyboard.isDown("w") then
+        y_diff = -1
+      end
+      if love.keyboard.isDown("s") then
+        y_diff = 1
+      end
+      if love.keyboard.isDown("a") then
+        x_diff = -1
+      end
+      if love.keyboard.isDown("d") then
+        x_diff = 1
+      end
+
+      spriteLayer.sprites.player.x = spriteLayer.sprites.player.x + x_diff
+      spriteLayer.sprites.player.y = spriteLayer.sprites.player.y + y_diff
+    end
+
+    function spriteLayer:draw()
+        for _, sprite in pairs(self.sprites) do
+            local x = math.floor(sprite.x)
+            local y = math.floor(sprite.y)
+            love.graphics.draw(sprite.image, x, y, 0)
+        end
+    end
 end
 
 function love.update(dt)
-  if love.keyboard.isDown("w") then
-
-  end
+    map:update(dt)
 end
 
-function love.draw(dt)
-  love.graphics.print("Hello World", player.x, player.y)
+function love.draw()
+    love.graphics.scale(3)
+    map:setDrawRange(0, 0, windowWidth, windowHeight)
+
+    map:draw()
 end
