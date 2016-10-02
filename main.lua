195f961e3e7e274f5ba4c3cbc7bf0d8920c40a2c
Sean Johnston <sean.c.johnston@gmail.com>
2016-10-02 13:16:21 +0100

    change assets

--- main.lua
+++ main.lua
@@ -1,42 +1,48 @@
 local sti = require "sti"
 
 function love.load()
+    y_move = 0
     windowWidth  = love.graphics.getWidth()
     windowHeight = love.graphics.getHeight()
 
-    map = sti("assets/testmap.lua")
+    map = sti("assets/map.lua")
     map:addCustomLayer("Sprite Layer", 3)
 
     local spriteLayer = map.layers["Sprite Layer"]
 
+    local player_img = love.graphics.newImage("assets/player.png")
+    player_img:setFilter("nearest", "nearest")
+
     spriteLayer.sprites = {
         player = {
-            image = love.graphics.newImage("assets/player.png"),
+            image = player_img,
             x = 64,
             y = 64,
             r = 0,
         }
     }
 
+    function love.keypressed(key, scancode, isrepeat)
+      local step = 8
+      local player_y = spriteLayer.sprites.player.y
+      local player_x = spriteLayer.sprites.player.x
+
+      if key == "w" and isrepeat == false then
+        spriteLayer.sprites.player.y = player_y - step
+      end
+      if key == "s" and isrepeat == false then
+        spriteLayer.sprites.player.y = player_y + step
+      end
+      if key == "a" and isrepeat == false then
+        spriteLayer.sprites.player.x = player_x - step
+      end
+      if key == "d" and isrepeat == false then
+        spriteLayer.sprites.player.x = player_x + step
+      end
+    end
+
     function spriteLayer:update(dt)
-      local y_diff = 0
-      local x_diff = 0
 
-      if love.keyboard.isDown("w") then
-        y_diff = -1
-      end
-      if love.keyboard.isDown("s") then
-        y_diff = 1
-      end
-      if love.keyboard.isDown("a") then
-        x_diff = -1
-      end
-      if love.keyboard.isDown("d") then
-        x_diff = 1
-      end
-
-      spriteLayer.sprites.player.x = spriteLayer.sprites.player.x + x_diff
-      spriteLayer.sprites.player.y = spriteLayer.sprites.player.y + y_diff
     end
 
     function spriteLayer:draw()
